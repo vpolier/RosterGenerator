05fe957b6e1d4ce79745cf7756d4075e7ff42bc2
vpolier <vpolier81@gmail.com>
2023-02-12 14:48:33 +1000

    set up login

---
+++ ys-rostergenerator/src/App.js
@@ -0,0 +1,63 @@
+import logo from './logo.svg';
+import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import React, { useState } from "react";
+import { Header } from "./Header.js";
+import { Login } from "./Login.js";
+import { Register } from "./Register.js";
+import { Roster } from "./Roster.js";
+import { AddShiftForm } from "./components/AddShiftForm.js";
+
+import Auth from './utils/auth';
+import { NavLink } from 'react-router-dom';
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+this.handleLogin = this.handleLogin.bind(this);
+this.handleLogout = this.handleLogout.bind(this);
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
+  return (
+    <div className="App">
+      <ApolloProvider client={client}>
+      <Header/>
+    {Auth.loggedIn() ? (
+        <Roster onFormSwitch={toggleForm} />
+      ): (
+        currentForm === "register" ? <Register onFormSwitch={toggleForm} /> : <Login onFormSwitch={toggleForm} />
+      )}
+      </ApolloProvider>
+    </div>
+  );
+
+}
+export default App;
